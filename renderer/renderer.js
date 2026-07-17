c6bd0e6608f613abd5bac66018a5ea07413240e8
NovaTune <novatune@release.local>
2026-07-17 22:40:16 +0300

    update: renderer.js — 22:40:15

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -9000,7 +9000,7 @@ function renderHelp() {
         </div>
       </div>
 
-      <div class="section-panel" id="help-updates-section">
+      <div class="section-panel" id="help-updates-section" style=>
           <div class="section-panel-title">What's New in v1.1.2</div>
           <div class="help-item">
             <div class="help-item-title">UI & Navigation Refinements</div>
