5de9e167353c47221f533de751644b76b6021fbd
NovaTune <novatune@release.local>
2026-07-19 11:01:58 +0300

    update: renderer.js — 11:01:57

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -350,7 +350,7 @@ class SquigglyProgress {
     this.strokeWidth = opts.strokeWidth ?? 2;
     this.thumbWidth = opts.thumbWidth ?? 10.6;
     this.thumbHeight = opts.thumbHeight ?? 5.6;
-    this.thumbRadius = opts.thumbRadius ?? ;
+    this.thumbRadius = opts.thumbRadius ?? 4;
 
     // Resolve wave color eagerly so we can pass it to the Worker
     this.waveColor =
