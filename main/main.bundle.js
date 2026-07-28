896790f37b7fef3d81f19a896d71abd23b49101c
NovaTune <novatune@release.local>
2026-07-28 12:24:50 +0300

    update: renderer.js — 12:24:49

--- main/main.bundle.js
+++ main/main.bundle.js
@@ -37810,7 +37810,7 @@ var require_package = __commonJS({
   "package.json"(exports2, module2) {
     module2.exports = {
       name: "novatune",
-      version: "1.1.2",
+      version: "1.1.3",
       description: "NovaTune \u2014 A premium Windows music player with Spotify-dark aesthetics",
       main: "main/main.bundle.js",
       scripts: {
